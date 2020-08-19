357f122e2a12256a29c482946dd1692666bc499c
Renato Moschetta <renatorock3@hotmail.com>
2020-08-19 20:42:56 -0300

    Update in GoRestaurant

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -32,7 +32,8 @@ const Orders: React.FC = () => {
 
   useEffect(() => {
     async function loadOrders(): Promise<void> {
-      // Load orders from API
+      const ordersResponse = await api.get('orders');
+      setOrders(ordersResponse.data);
     }
 
     loadOrders();
